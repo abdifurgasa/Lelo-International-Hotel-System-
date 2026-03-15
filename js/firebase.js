ba963f228f762f2ce7c198e959bb6912a205291f
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-03-15 13:44:57 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,18 +1,37 @@
-// Import Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+// Import Firebase core
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+// Import Analytics
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+
+// Import Auth
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+// Import Firestore
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+// Import Storage
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBQOT84qzCDIk5YrgrUD-SeXaNV5HwpBVE",
-  authDomain: "lelo-international-hotel-2254a.firebaseapp.com",
-  projectId: "lelo-international-hotel-2254a",
-  storageBucket: "lelo-international-hotel-2254a.firebasestorage.app",
-  messagingSenderId: "889193332993",
-  appId: "1:889193332993:web:6ca3745ebbb05a75939b00",
-  measurementId: "G-C1M5H8T741"
+  apiKey: "AIzaSyDE2bQuZ7kk7kDBIcfsg-GD-LvFoM0dPWg",
+  authDomain: "lelo-intarnational-hotel.firebaseapp.com",
+  projectId: "lelo-intarnational-hotel",
+  storageBucket: "lelo-intarnational-hotel.firebasestorage.app",
+  messagingSenderId: "509777274669",
+  appId: "1:509777274669:web:393d55c77333420d2315a4",
+  measurementId: "G-77PG7GMYQ4"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Services
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+// Export services
+export { app, analytics, auth, db, storage };
